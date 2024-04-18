b50364add3f36f25e249ff4c37d4427b5cf4e8f7
Hridayansh018 <hridayansh018@gmail.com>
2024-04-19 01:20:15 +0530

    Practice Program 3 update

---
+++ prog3.py
@@ -0,0 +1,8 @@
+s1 = str(input("Enter String:- ")) # COMPUTER
+s2 = str(input("Enter String:- ")) # PROGRAMMING
+s3 = str(s1+" "+s2)
+
+print("Added String is",s3)
+print("Substring of s1 is",s1[1:5])
+print("Substring of s2 is",s2[2:6])
+print("Substring of Added String is",s3[1:8])
